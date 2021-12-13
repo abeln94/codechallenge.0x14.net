aee4386cc0c67c809d29033a3d2e0e43b09a560c
abeln94 <abeln94@gmail.com>
2021-12-13 22:36:22 +0100

    4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,69 +1,75 @@
 import java.io.File
+import java.lang.Exception
 
 fun main() {
     // prepare output file
     File("output.txt").printWriter().use { out ->
-        var index = 0
+        var index = 1
         // prepare input file
-        File("input.txt").forEachLine { line ->
+        File("input.txt").readLines().drop(1).windowed(2, 2) { (f, s) ->
             // convert each
-            if (index != 0) out.println("Case #$index: " + convert(line))
+            out.println("Case #$index: " + convert(f, s))
             index++
         }
     }
 }
 
 /**
- * Convert each line
+ * list of notes
  */
-fun convert(line: String): String {
-    // split first by '|' (with a bit of cleanup for unused chars)
-    val (words, points_raw) = line.remove(" ").remove("'").split('|')
+val notes = listOf(
+    listOf("A"),
+    listOf("A#", "Bb"),
+    listOf("B", "Cb"),
+    listOf("C", "B#"),
+    listOf("C#", "Db"),
+    listOf("D"),
+    listOf("D#", "Eb"),
+    listOf("E", "Fb"),
+    listOf("F", "E#"),
+    listOf("F#", "Gb"),
+    listOf("G"),
+    listOf("G#", "Ab"),
+)
 
-    val (ignore, element_sep, data_sep) = when {
-        // if dictionary: ignore '{...}', split by "," then by ":"
-        points_raw.startsWith('{') -> Triple(1, ",", ":")
-        // if tuple: ignore '[(...)]', split by "," then by ":"
-        points_raw.startsWith('[') -> Triple(2, "),(", ",")
-        // if assignments: ignore nothing, split by "," then by "="
-        else -> Triple(0, ",", "=")
-    }
-
-    val points = points_raw
-        // ignore
-        .substring(ignore, points_raw.length - ignore)
-        // separation of elements
-        .split(element_sep)
-        // separation of char and value
-        .map { it.split(data_sep) }
-        // convert to map
-        .associate { (c, p) ->
-            // associating to each char its score
-            c[0] to
-                    // fractions are just a division
-                    if ('/' in p) p.split('/').map { it.toDouble() }.let { (l, r) -> l / r }
-                    // normal numbers are better
-                    else p.toDouble()
+/**
+ * Convert each step
+ */
+fun convert(root: String, scale: String): String {
+    // take each scale numerically
+    val notes = scale.map { if (it == 'T') 2 else 1 }
+        // and convert to the list of steps (numerical)
+        .fold(listOf(notes.indexOfFirst { root in it })) { result, step ->
+            // traversing the array
+            result + ((result.last() + step) % notes.size)
         }
-
-    // calculate the score of the two words
-    val (pair1, pair2) = words.split("-").map {
-        // by adding all its points
-        it to it.toCharArray().sumOf { c -> points[c] ?: 0.0 }
-    }
-
-    // and return max
-    return when {
-        pair1.second > pair2.second -> pair1.first
-        pair1.second < pair2.second -> pair2.first
-        // unless they are equal
-        else -> "-"
-    }
-
+        // then to the list of possible notes
+        .map { notes[it] }
+    // to find a valid solution, we use a recursive approach
+    return foldUnique(emptyList(), notes) ?: throw Exception("no solution")
 }
 
 /**
- * Just a shorthand of replacing something with nothing
+ * Takes an already built result, and tries to append one of each in notes.
+ * If there is a solution, it is returned, if not null is returned
  */
-private fun String.remove(s: String) = replace(s, "")
+fun foldUnique(result: List<String>, notes: List<List<String>>): String? {
+    // solution! return it
+    if (notes.isEmpty()) return result.joinToString("")
+
+    // for each in the next possible notes
+    notes.first()
+        // remove those who can't satisfy the current solution
+        .filter { possible -> result.none { it != possible && it[0] == possible[0] } }
+        // and try all the others
+        .forEach { possible ->
+            // try it
+            foldUnique(result + possible, notes.drop(1))
+                // if valid, return
+                ?.let { return it }
+            // else continue
+        }
+    // no solution
+    return null
+}
 
