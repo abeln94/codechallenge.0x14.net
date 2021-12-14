7ec5ef6d0bc2c65d4485c83f17f70042c7ae5378
abeln94 <abeln94@gmail.com>
2021-12-14 22:46:48 +0100

    8 pass

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,132 +1,38 @@
 import java.io.BufferedReader
 import java.io.File
-import java.io.InputStreamReader
-import java.io.PrintWriter
-import java.net.Socket
-import java.util.*
-
-//----------------- socket -----------------//
-
-// prepare
-val pingSocket = Socket("codechallenge-daemons.0x14.net", 4321)
-val out = PrintWriter(pingSocket.getOutputStream(), true)
-val input = BufferedReader(InputStreamReader(pingSocket.getInputStream()))
-
-/**
- * Close socket
- */
-fun closeAll() {
-    input.close()
-    out.close()
-    pingSocket.close()
-}
-
-/**
- * sends a command, returns the result
- */
-fun communicate(data: String): String {
-    // log too
-    println(data)
-    out.println(data)
-    return input.readLine().also { println(it) }
-}
-
-/**
- * Read all available lines
- */
-fun readReady() = buildString {
-    Thread.sleep(100) // give a little rest
-    while (input.ready()) append(input.readLine())
-}
-
-//----------------- labyrinth -----------------//
-
-// visited cells, to avoid repeating
-val visited = mutableSetOf<Pair<Int, Int>>()
-
-// a state of the process
-data class State(val x: Int, val y: Int, val direction: String?, val path: List<String>)
-
-// states to check, fifo list (so that the solution is the shortest).
-val states = ArrayDeque<State>()
-
-/**
- * Solves the labyrinth
- */
-fun labyrinth() {
-    // skip introduction
-    readReady()
-    // initialize
-    states.add(State(0, 0, null, mutableListOf()))
-    // check all states in order
-    while (states.isNotEmpty()) {
-        // get next position
-        var (x, y, direction, path) = states.remove()
-
-        // traverse where needed (unless this is the first state)
-        if (direction != null) {
-            // calculate new cell
-            val (nx, ny) = when (direction) {
-                "west" -> x + 1 to y
-                "east" -> x - 1 to y
-                "north" -> x to y + 1
-                "south" -> x to y - 1
-                else -> throw Exception()
-            }
-            // skip if already visited
-            if (nx to ny in visited) continue
-            // go there
-            communicate("go to $x,$y")
-            communicate(direction)
-            println("I think I'm in $nx $ny")
-            x = nx
-            y = ny
-        }
-        // mark as visited and calculate new path
-        visited += x to y
-        val newPath = path + "($x,$y)"
-
-        // check exit
-        if (!communicate("is exit?").startsWith("No")) {
-            // exit! log path and exit
-            File("output.txt").printWriter().use { out ->
-                out.write(newPath.joinToString(", "))
-            }
-            return
-        }
-        // no exit, look and prepare next states
-        communicate("look").substringAfterLast(": ").split(" ")
-            .forEach { newDirection ->
-                states.add(State(x, y, newDirection, newPath))
-            }
-
-    }
-}
-
-//----------------- testing -----------------//
-
-/**
- * The most basic interactive prompt
- */
-fun prompt() {
-    while (true) {
-        println(readReady())
-        readLine().let {
-            when (it) {
-                // run the program, should be run while in 0,0
-                "_labyrinth" -> labyrinth()
-                // exit
-                "_exit" -> return
-                // send
-                else -> out.println(it)
-            }
-        }
-    }
-}
-
-//----------------- main -----------------//
 
 fun main() {
-    prompt()
-    closeAll()
+    // prepare output file
+    File("output.txt").printWriter().use { out ->
+        var index = 1
+        // prepare input file
+        File("input.txt").bufferedReader().use { inp ->
+            // each case
+            (1..inp.readLine().toInt()).forEach { n ->
+                // read data
+                val t = inp.readLine().toInt()
+                val tickets = inp.readLines(t).map { it.split(",").let { (f, s) -> f to s } }
+                val cities = tickets.flatMap { it.toList() }.distinct()
+
+                // Creating first example graph
+                val V = cities.size
+                val adj1 = ArrayList<ArrayList<Int>>()
+                for (i in 0 until V) adj1.add(ArrayList())
+                tickets.forEach { (a, b) ->
+                    Graph.addEdge(adj1, cities.indexOf(a),cities.indexOf(b))
+                }
+                val result = Graph.AP(adj1, V).map { cities[it] }.sorted().takeIf { it.isNotEmpty() }?.joinToString(",") ?: "-"
+
+                // report output
+                out.println("Case #${index++}: $result")
+            }
+        }
+    }
 }
+
+/**
+ * Read multiple lines
+ */
+private fun BufferedReader.readLines(p: Int) = (1..p).map { readLine() }.toMutableList()
+
+
