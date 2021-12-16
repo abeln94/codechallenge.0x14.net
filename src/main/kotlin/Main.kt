0af89fb0a1c3afced036de77b5b68093ee094e20
abeln94 <abeln94@gmail.com>
2021-12-16 19:30:13 +0100

    12

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,11 @@ import java.io.BufferedReader
 import java.io.File
 import kotlin.math.max
 
+/**
+ * Bitcoin
+ */
+const val BTC = "BTC"
+
 fun main() {
     // prepare output file
     File("output.txt").printWriter().use { out ->
@@ -11,45 +16,63 @@ fun main() {
             // for each case
             (1..inp.readLine().toInt()).forEach { index ->
 
-                val tradings = mutableMapOf<String, MutableMap<String, Int>>()
+                // prepare trading dictionary of better trades (fromType -> toType:multiplier)
+                // its default value is an empty map, no trades
+                val tradings = mutableMapOf<String, MutableMap<String, Int>>().withDefault { mutableMapOf() }
 
                 // for each website
                 (1..inp.readLine().toInt()).forEach {
-                    val trades = inp.readLine().substringAfterLast(" ").toInt()
 
-                    (1..trades).forEach {
+                    // and for each trade it offers
+                    (1..inp.readLine().substringAfterLast(" ").toInt()).forEach {
+                        // get and parse the data
                         val (fromType, s_multiplier, toType) = inp.readLine().split("-")
                         val multiplier = s_multiplier.toInt()
+
+                        // get the trading
                         tradings.getOrPut(fromType) {
+                            // if didn't exist, initialize with a new one
                             mutableMapOf<String, Int>().withDefault { 0 }
-                        }.let {
-                            if (multiplier > it.getValue(toType)) it[toType] = multiplier
+                        }.let { trade ->
+                            // if the multiplier is greater than what was saved, replace it
+                            // note that the default is 0, so we never save a 0 trade
+                            if (multiplier > trade.getValue(toType)) trade[toType] = multiplier
                         }
                     }
                 }
 
+                // prepare a list of current testing trades
+                var testing = listOf(BTC to 1)
 
-                var testing = listOf("BTC" to 1)
-
+                // and a list of already tested trades (to avoid infinite loops and unnecessary tests)
                 val tested = testing.toMutableList()
 
+                // keep the best currently found trade
                 var result = 1
 
+                // and iterate while there are trades to test, and no improved result
                 while (testing.isNotEmpty() && result == 1) {
+                    // for all current testing trades, get the new ones and replace them
                     testing = testing.flatMap { (fromType, value) ->
-                        tradings[fromType]?.let {
-
-                            it.map { (toType, multiplier) ->
+                        // get all next trades (if any)
+                        tradings.getValue(fromType)
+                            // and generate the new trades
+                            .map { (toType, multiplier) ->
                                 toType to value * multiplier
-                            }.filter { it !in tested }
-                                .also { tested.addAll(it) }
-                        } ?: emptyList()
+                            }
+                            // keep distinct to avoid unnecessary computations
+                            .distinct()
+                            // remove those who were already tested (if they happened before, they were better)
+                            .filter { it !in tested }
+                            // and mark the remaining as tested
+                            .also { tested.addAll(it) }
                     }
-                    testing.maxByOrNull { it.first == "BTC" }?.second?.let {
+                    // check if there is a BTC result which is better than the current best trade
+                    testing.maxByOrNull { it.first == BTC }?.second?.let {
+                        // if there is, update it
                         result = max(it, result)
                     }
                 }
-
 
                 // report output
                 out.println("Case #${index}: $result")
