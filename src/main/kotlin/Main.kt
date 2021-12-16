1397f4ac57008806ca9e2ab6bc091bbb5cb767ed
abeln94 <abeln94@gmail.com>
2021-12-16 18:28:19 +0100

    11

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -18,9 +18,11 @@ fun main() {
                 // while there are still functions unmatched
                 while (unmatched.isNotEmpty()) {
                     // get the best group
-                    val matched = unmatched.takeBestOfSize(k)
+                    val (matched, prefixSize) = unmatched.takeBestOfSize(k)
                     // add its points
-                    points += matched.reduce { l, r -> l.commonPrefixWith(r) }.length
+                    points += prefixSize
+                    if (matched.reduce { l, r -> l.commonPrefixWith(r) }.length != prefixSize)
+                        unmatched.takeBestOfSize(k)
                     // remove from the unmatched
                     // [kotlin issue: unmatched.removeAll removes duplicates, we don't want that]
                     matched.forEach { unmatched.remove(it) }
@@ -34,15 +36,29 @@ fun main() {
 }
 
 /**
- * returns [size] strings of this list with the bigger common prefix
+ * returns [size] strings of this list with the bigger common prefix, and the length of that common prefix
  * All chars before (not including) [char] should be equal already
  */
-tailrec fun List<String>.takeBestOfSize(size: Int, char: Int = 0): Pair<List<String>> =
-    groupBy { it.getOrElse(char) { ' ' } }.values
-        .filter { it.size >= size }
-        .getOrElse(0) { return take(size) to char }
-        .also { if (it[0].length < char) return it.take(size) }
-        .takeBestOfSize(size, char + 1)
+tailrec fun List<String>.takeBestOfSize(size: Int, char: Int = 0): Pair<List<String>, Int> =
+    // first group by next character to check (if the string is shorter it is considered to be followed by a space)
+    groupBy { it.getOrElse(char) { ' ' } }.entries
+        // keep only those groups containing [size] or more entries
+        .filter { it.value.size >= size }
+        // get the first group (although it doesn't really matter which one you get)
+        .getOrElse(0) {
+            // if there are no groups, it means there are no [size] strings with common prefix in this step,
+            // so in that case return any [size] one of them. The common prefix length is exactly [char]
+            return take(size) to char
+        }
+        // now check if the common char is the space
+        .also {
+            // if so, it means there is no need to do further checks and we can exit now
+            // the common prefix is exactly [char] again
+            if (it.key == ' ') return it.value.take(size) to char
+        }
+        // otherwise, we have a group with at least [size] strings,
+        // so repeat to find the better group there from the next char
+        .value.takeBestOfSize(size, char + 1)
 
 
 /**
