1f8256d79210f8cdc5b129adcfcddeac30835304
abeln94 <abeln94@gmail.com>
2021-12-16 19:21:02 +0100

    12 test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 import java.io.BufferedReader
 import java.io.File
+import kotlin.math.max
 
 fun main() {
     // prepare output file
@@ -10,56 +11,52 @@ fun main() {
             // for each case
             (1..inp.readLine().toInt()).forEach { index ->
 
-                // get data
-                val (n, k) = inp.readLine().splitInts()
-                val unmatched = inp.readLines(n)
-                var points = 0
+                val tradings = mutableMapOf<String, MutableMap<String, Int>>()
 
-                // make the groups by continuously extracting k strings with the longer prefix
-                // until no more strings are left
+                // for each website
+                (1..inp.readLine().toInt()).forEach {
+                    val trades = inp.readLine().substringAfterLast(" ").toInt()
 
-                // while there are still functions unmatched
-                while (unmatched.isNotEmpty()) {
-                    // get the best group
-                    val (matched, prefixSize) = unmatched.takeBestOfSize(k)
-                    // add its points
-                    points += prefixSize
-                    // remove from the unmatched (if there are duplicates, remove only one)
-                    matched.forEach { unmatched.remove(it) }
+                    (1..trades).forEach {
+                        val (fromType, s_multiplier, toType) = inp.readLine().split("-")
+                        val multiplier = s_multiplier.toInt()
+                        tradings.getOrPut(fromType) {
+                            mutableMapOf<String, Int>().withDefault { 0 }
+                        }.let {
+                            if (multiplier > it.getValue(toType)) it[toType] = multiplier
+                        }
+                    }
                 }
 
+
+                var testing = listOf("BTC" to 1)
+
+                val tested = testing.toMutableList()
+
+                var result = 1
+
+                while (testing.isNotEmpty() && result == 1) {
+                    testing = testing.flatMap { (fromType, value) ->
+                        tradings[fromType]?.let {
+
+                            it.map { (toType, multiplier) ->
+                                toType to value * multiplier
+                            }.filter { it !in tested }
+                                .also { tested.addAll(it) }
+                        } ?: emptyList()
+                    }
+                    testing.maxByOrNull { it.first == "BTC" }?.second?.let {
+                        result = max(it, result)
+                    }
+                }
+
+
                 // report output
-                out.println("Case #${index}: $points")
+                out.println("Case #${index}: $result")
             }
         }
     }
 }
-
-/**
- * returns [size] strings of this list with the bigger common prefix, and the length of that common prefix
- * All chars before (not including) [char] should be equal already
- */
-tailrec fun List<String>.takeBestOfSize(size: Int, char: Int = 0): Pair<List<String>, Int> =
-    // first group by next character to check (if the string is shorter it is considered to be followed by a space)
-    groupBy { it.getOrElse(char) { ' ' } }.entries
-        // keep only those groups containing [size] or more entries
-        .filter { it.value.size >= size }
-        // get the first group (although it doesn't really matter which one you get)
-        .getOrElse(0) {
-            // if there are no groups, it means there are no [size] strings with common prefix in this step,
-            // so in that case return any [size] one of them. The common prefix length is exactly [char]
-            return take(size) to char
-        }
-        // now check if the common char is the space
-        .also {
-            // if so, it means there is no need to do further checks and we can exit now
-            // the common prefix is exactly [char] again
-            if (it.key == ' ') return it.value.take(size) to char
-        }
-        // otherwise, we have a group with at least [size] strings,
-        // so repeat to find the better group there from the next char
-        .value.takeBestOfSize(size, char + 1)
-
 
 /**
  * Splits a string into a space separated list of integers
