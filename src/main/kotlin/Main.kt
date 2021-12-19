d601f404c324a9a56ea025369a9c0588f2a5bd6d
abeln94 <abeln94@gmail.com>
2021-12-19 17:36:11 +0100

    20 solved first

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,107 +1,118 @@
-import java.io.BufferedReader
+import java.awt.Color
+import java.awt.image.BufferedImage
 import java.io.File
-import java.math.BigInteger
+import javax.imageio.ImageIO
 
-/**
- * Algorithm:
- * After reading and understanding the problem, the solution is as follows:
- * We are required to find the bigger polynomial that allows us to not modify the crc when applying some noise
- * This is the equivalent as saying we need to find the bigger polynomial that zeros (when applying crc) the noise
- * (because CRC(message^noise^0..0)=CRC(message)^CRC(noise)^CRC(0..0), and since CRC(noise)=0 then CRC(message^noise)=CRC(message)
- *
- * We need to find the bigger polynomial that 'zeroes' all the noise.
- * The CRC operation is just as a 'simple' division, where the result is the CRC output,
- * and so we 'simply' need to find the gcd (great common divisor) of all the polynomial noises,
- * which can be calculated sequentially gcd(1,2,3) = gcd(gcd(1,2),3) etc
- * And to find the gcd, we can use the Euclidean algorithm which just so happens to only require the modulo operation
- * Exactly what we know how to do! (CRC)
- *
- * So, to recap:
- * 1) Get all the noise
- * 2) Apply gcd to each pair of noise until only one is left
- *    a) to apply gcd use the euclidean algorithm using crc itself as the modulo operation
- * 3) The result is the bigger polynomial that solves the problem
- *
- * Except there is one extra requirement: the polynomial must end in '1'
- * But that's easy, we just need to remove the extra leading zeros,
- * because a polynomial still works when adding or subtracting zeros (if the length fits)
- *
- * so
- * 4) Remove all leading zeros of the output
- */
 
 fun main() {
-    // prepare output file
-    File("output.txt").printWriter().use { out ->
-        // prepare input file
-        File("input.txt").bufferedReader().use { inp ->
+    val img = ImageIO.read(File("hidden_toy_story.png"))
 
-            // for each case
-            (1..inp.readLine().toInt()).forEach { index ->
+    val visited = mutableSetOf<Pair<Int, Int>>()
 
-                // read the numbers, and store as bigint
-                val numbers = inp.readLines(inp.readLine().toInt()).map { BigInteger(it, 16) }
+    var x = 81
+    var y = 114
 
-                // compute the gcd of all numbers, by doing gcd of 1 with 2, then result with 3, etc (until only one is left)
-                var result = numbers.reduce { a, b -> gcd(a, b) }
+    val reds = mutableListOf<Int>()
+    val greens = mutableListOf<Int>()
+    val blues = mutableListOf<Int>()
 
-                // the required polynomial need to end in 1 by removing leading zeros
-                // with bigintegers that's just divide by 2
-                while (result>BigInteger.ONE && result.mod(BigInteger.TWO) == BigInteger.ZERO)
-                    result = result.divide(BigInteger.TWO)
+    val result = buildString {
 
-                // report output, just write it in lowercase hex again
-                out.println("Case #${index}: ${result.toString(16).lowercase()}")
+        fun aprint(a: Any?) = print(a).also { append(a) }
+
+        img@ while (true) {
+
+            visited += x to y
+            val red = img.red(x, y)
+            val green = img.green(x, y)
+            val blue = img.blue(x, y)
+
+//        println("$x,$y -> $red $green $blue")
+
+//            aprint(red.binary(8))
+//            print(" ")
+//            aprint(green.binary(8))
+//            print(" ")
+//            aprint(blue.binary(8))
+//            println()
+
+            aprint((red - 72).binary(1))
+            aprint((green - 72).binary(1))
+            aprint((blue - 72).binary(1))
+
+//            if (reds.isNotEmpty()) {
+//                aprint(((red > (reds.last()))).bin)
+//                aprint(((green > (greens.last()))).bin)
+//                aprint(((blue > (blues.last()))).bin)
+//            }
+
+//            val d = 75
+//            aprint(if (red != d) "1" else "0")
+//            aprint(if (blue != d) "1" else "0")
+//            aprint(if (green != d) "1" else "0")
+//        aprint((blue != green).bin)
+//            aprint((green % 2 == 0).bin)
+//            aprint((green == 77).bin)
+
+            reds += red
+            greens += green
+            blues += blue
+
+            for ((dx, dy) in listOf(0 to 1, 1 to 0, 0 to -1, -1 to 0, 1 to -1, 1 to 1, -1 to 1, -1 to -1)) {
+                val i = x + dx
+                val j = y + dy
+                if (i == x && j == y) continue
+                if (img.red(i, j) > 100) continue
+                if ((i to j) in visited) continue
+
+                x = i
+                y = j
+                continue@img
             }
+            break@img
         }
-    }
-}
 
-/**
- * The gcd (for crc) of two bigintegers
- * Just the Euclidean algorithm
- */
-fun gcd(a: BigInteger, b: BigInteger): BigInteger {
-
-    var (small, big) =
-        if (a < b) a to b
-        else b to a
-
-    while (true) {
-        val mod = division(big, small)
-        if (mod == BigInteger.ZERO) break
-        big = small
-        small = mod
     }
 
-    return small
+    val inverted = result.map { (it == '0').bin }.joinToString("")
+    println()
+    println(result.asBinary())
+    println()
+    println(inverted)
+    println(inverted.asBinary())
+    println()
+    println(result.reversed())
+    println(result.reversed().asBinary())
+    println()
+    println(inverted.reversed())
+    println(inverted.reversed().asBinary())
+
+    println()
+    println(reds.histogram())
+    println(greens.histogram())
+    println(blues.histogram())
+    println(visited.size)
+
+//    (visited.minOf { it.second }..visited.maxOf { it.second }).forEach { y ->
+//        (visited.minOf { it.first }..visited.maxOf { it.first }).forEach { x ->
+//            print(if (x to y in visited) "#" else " ")
+//        }
+//        println()
+//    }
+
 }
 
-/**
- * The CRC division
- */
-fun division(big: BigInteger, small: BigInteger): BigInteger {
-    // prepare the divider
-    val divider = small.toString(2).toCharArray()
-    // and the data (with appended zeros at the end for the result)
-    var data = (big.toString(2) + "0".repeat(divider.size - 1)).toCharArray()
-    // now keep dividing while required
-    while (data.size > divider.size - 1) {
-        if (data[0] == '0') {
-            // skip directly if the first byte is 0
-            data = data.drop(1).toCharArray()
-        } else {
-            // do the xor otherwise
-            divider.forEachIndexed { i, b ->
-                data[i] = if (data[i] != b) '1' else '0' // this is a xor
-            }
-        }
-    }
-    // parse and return the result
-    return BigInteger(data.joinToString(""), 2)
-}
+private fun String.asBinary() =
+    windowed(8, 8).map { Integer.parseInt(it, 2).toChar() }.joinToString("")
 
-/**
- * Read multiple lines
- */
-private fun BufferedReader.readLines(p: Int) = (1..p).map { readLine() }.toMutableList()
+
+private fun <T : Comparable<T>> Iterable<T>.histogram() =
+    groupBy { it }.mapValues { it.value.size }.entries.sortedBy { it.key }
+
+
+fun BufferedImage.red(x: Int, y: Int) = Color(getRGB(x, y)).red
+fun BufferedImage.green(x: Int, y: Int) = Color(getRGB(x, y)).green
+fun BufferedImage.blue(x: Int, y: Int) = Color(getRGB(x, y)).blue
+
+fun Int.binary(length: Int) = toBigInteger().toString(2).padStart(length, '0').takeLast(length)
+val Boolean.bin get() = if (this) '1' else '0'
