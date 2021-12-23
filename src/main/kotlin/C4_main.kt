a8c269ab9c0f04871494dce5de075ea5db4285bb
abeln94 <abeln94@gmail.com>
2021-12-23 20:08:39 +0100

    C4 ready for submission

---
+++ src/main/kotlin/C4_main.kt
@@ -0,0 +1,81 @@
+import java.io.File
+
+fun main() {
+    run("sample")
+    run("test")
+    run("submit")
+}
+
+private fun run(data: String) {
+    // prepare output file
+    File("C4_${data}_out.txt").printWriter().use { out ->
+        var index = 1
+        // prepare input file
+        File("C4_${data}_in.txt").readLines().drop(1).windowed(2, 2) { (f, s) ->
+            // convert each
+            out.println("Case #$index: " + convert(f, s))
+            index++
+        }
+    }
+}
+
+/**
+ * list of notes
+ */
+private val notes = listOf(
+    listOf("A"),
+    listOf("A#", "Bb"),
+    listOf("B", "Cb"),
+    listOf("C", "B#"),
+    listOf("C#", "Db"),
+    listOf("D"),
+    listOf("D#", "Eb"),
+    listOf("E", "Fb"),
+    listOf("F", "E#"),
+    listOf("F#", "Gb"),
+    listOf("G"),
+    listOf("G#", "Ab"),
+)
+
+/**
+ * Convert each step
+ */
+private fun convert(root: String, scale: String): String {
+    // take each scale numerically
+    val notes = scale.map { if (it == 'T') 2 else 1 }
+        // and convert to the list of steps (numerical)
+        .fold(listOf(notes.indexOfFirst { root in it })) { result, step ->
+            // traversing the array
+            result + ((result.last() + step) % notes.size)
+        }
+        // then to the list of possible notes
+        .map { notes[it] }
+    // to find a valid solution, we use a recursive approach
+    return foldUnique(emptyList(), notes) ?: throw Exception("no solution")
+//    return foldUnique(listOf(root), notes.drop(1)) ?: throw Exception("no solution") // replace the previous line with this one to 'fix' the problem
+}
+
+/**
+ * Takes an already built result, and tries to append one of each in notes.
+ * If there is a solution, it is returned, if not null is returned
+ */
+fun foldUnique(result: List<String>, notes: List<List<String>>): String? {
+    // solution! return it
+    if (notes.isEmpty()) return result.joinToString("")
+
+    // for each in the next possible notes
+    notes.first()
+        // remove those who can't satisfy the current solution
+        .filter { possible -> result.none { it != possible && it[0] == possible[0] } }
+        // and try all the others
+        .forEach { possible ->
+            // try it
+            foldUnique(result + possible, notes.drop(1))
+                // if valid, return
+                ?.let { return it }
+            // else continue
+        }
+    // no solution
+    return null
+}
+
